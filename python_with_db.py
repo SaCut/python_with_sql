ebec63e05a25d71f83d225d1714a356373664799
Saverio <SaCut@users.noreply.github.com>
2021-03-30 11:41:20 +0100

    added code to python_with_db.py

--- python_with_db.py
+++ python_with_db.py
@@ -1,1 +1,17 @@
 # import PYODBC
+import pyodbc
+
+# let's establish the connection using pyodbc
+server = "18.135.103.95"
+
+database = "Northwind"
+
+username = "SA"
+
+password = "Passw0rd2018"
+
+docker_Northwind = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+
+cursor = docker_Northwind.cursor()
+
+print(cursor.execute("SELECT @@version;"))
